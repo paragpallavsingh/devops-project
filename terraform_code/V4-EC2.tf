afc63db136c8cd47c904e97710a7c261188646da
Parag Pallav Singh <thakurparag6.1@gmail.com>
2023-09-26 01:05:44 +0530

    change pem file and add ansible hosts to /etc/ansible

--- terraform_code/V4-EC2.tf
+++ terraform_code/V4-EC2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "demo-server" {
   ami = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
-  key_name = "k8s-hp"
+  key_name = "do-kp"
   //security_groups = ["demo-sg"]
   vpc_security_group_ids = [ aws_security_group.demo-sg.id]
   subnet_id = aws_subnet.demo-public-snet-1.id
