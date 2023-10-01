1d6a049b1a10164113f06efee439f44ef8c0cc29
Parag Pallav Singh <thakurparag6.1@gmail.com>
2023-10-01 19:19:03 +0530

    udapte eks

--- terraform_code/eks/eks.tf
+++ terraform_code/eks/eks.tf
@@ -118,7 +118,7 @@ resource "aws_iam_instance_profile" "worker" {
 
 ###############################################################################################################
 resource "aws_eks_cluster" "eks" {
-  name = "pps -eks-01"
+  name = "pps-eks-01"
   role_arn = aws_iam_role.master.arn
 
   vpc_config {
